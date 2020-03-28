e2847f6a9bb571c92de65670eeeea9fba630ceb9
Ubuntu <ubuntu@ip-172-31-44-8.us-west-2.compute.internal>
2020-03-28 13:20:20 +0000

    server training version, model--mobilenet_v2 and have no OHEM

---
+++ utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+import os
+import os.path as osp
+import argparse
+
+Config ={}
+Config['root_path'] = '/home/ubuntu/HW4/polyvore_outfits'
+Config['meta_file'] = 'polyvore_item_metadata.json'
+Config['checkpoint_path'] = 'checkpoint'
+
+
+Config['use_cuda'] = True
+Config['debug'] = False
+Config['num_epochs'] = 25
+Config['batch_size'] = 125
+
+Config['learning_rate'] = 0.001
+Config['num_workers'] = 8
+Config['lr_decay'] = 1
+
