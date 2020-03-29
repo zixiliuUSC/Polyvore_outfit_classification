d7925aa21cbbe2d2fb14e051a8704e4316df1ac7
zixiliuUSC <zixi@DESKTOP-HEENPF9.localdomain>
2020-03-29 16:22:02 +0800

    fix bugs

--- train_category.py
+++ train_category.py
@@ -57,8 +57,14 @@ def train_model(get_dataloader, model, criterion, optimizer, device, num_epochs,
                         num_inst = outputs.size(0)
                         num_hns = int(ratio * num_inst)
                         _, idxs = inst_losses.topk(num_hns) 
-                        loss = torch.mean(loss_inst.index_select(0, idxs))
+                        input1 = input1.index_select(0, idxs)
+                        input2 = input2.index_select(0, idxs)
+                        outputs = model(inputs1, input2)
+                        _,pred = torch.max(outputs,1)
                         criterion.reduction = 'mean'
+                        loss = criterion(outputs, labels.index_select(0, idxs))
+                        #loss = torch.mean(loss_inst.index_select(0, idxs))
+                        
                     else:
                         criterion.reduction = 'mean'
                         loss = criterion(outputs, labels)
